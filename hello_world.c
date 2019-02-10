f667e2c4a38eda792624120d53d161fc5c579c9f
Yan-XYR <amandax2013.ax@gmail.com>
2019-02-09 22:23:39 -0800

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -7,6 +7,19 @@
 int
 main(int argc, char const *argv[])
 {
-    printf("CSC415, This program has been written by %s!\n", MYNAME);
+    char smsg[128];
+    int bytes_formatted, bytes_written;
+    bytes_formatted = sprintf(smsg, "CSC415, This program has been written by %s!\n", MYNAME);
+    if(bytes_formatted < 0){
+        perror("sprintf failed");
+        return -45;
+    }
+
+    bytes_written = write(1,smsg,bytes_formatted);
+    if(bytes_written < 0){
+        perror("writted failed");
+        return -43;
+    }
+ 
 	return 0;
 }
